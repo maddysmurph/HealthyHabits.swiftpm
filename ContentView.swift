0ef519de672e27ea5a02a127c3f9655f2d0bc83d
Maddy Murphy <mmurphy6284@stu.d214.org>
2024-04-17 14:56:19 -0500

    Began Homepage views

--- ContentView.swift
+++ ContentView.swift
@@ -2,11 +2,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        Text("Welcome To...")
+            .font(.system(size: 30))
+            .bold()
+            .foregroundColor(.yellow)
+       Text("HEALTHY HABITS")
+            .font(.system(size: 45))
+            .bold()
+            .foregroundColor(.yellow)
+        Text("Track and Personalize Your Health Plan")
+            .bold()
+            .foregroundColor(.yellow)
     }
+}
+extension Color {
+    static let CustomYellowLight = Color (red: 218/255, green: 165/255, blue: 32/255)
+}
+extension Color {
+    static let CustomYellowDark = Color (red: 218/255, green: 165/255, blue: 32/255)
 }
