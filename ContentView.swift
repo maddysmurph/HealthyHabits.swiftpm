e5b9217db66d1dcd57f5455212b609be980c55e4
Nicole Park <ypark5275@stu.d214.org>
2024-04-19 14:49:28 -0500

    created welcome screen that says the app name and features.

--- ContentView.swift
+++ ContentView.swift
@@ -2,18 +2,54 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Welcome To...")
-            .font(.system(size: 30))
-            .bold()
-            .foregroundColor(.yellow)
-       Text("HEALTHY HABITS")
+        Text("Welcome to")
+            .font(.system(size: 40))
+            .foregroundColor(.black)
+       Text("Healthy Habits")
             .font(.system(size: 45))
             .bold()
-            .foregroundColor(.yellow)
-        Text("Track and Personalize Your Health Plan")
-            .bold()
-            .foregroundColor(.yellow)
-    }
+            .foregroundColor(.black)
+        Divider()
+            .padding(25.0)
+        VStack {
+            VStack {
+                HStack {
+                    Image(systemName: "fork.knife")
+                        .resizable()
+                        .frame(width: 35, height: 70)
+                    Text("Personalized goals")
+                        .font(.system(size: 25))
+                        .multilineTextAlignment(.leading)
+                        .bold()
+                        .foregroundColor(.black)
+                }
+                VStack {
+                    HStack {
+                        Image(systemName: "figure.run")
+                            .resizable()
+                            .frame(width: 45, height: 60)
+                        Text("Track your activity")
+                            .font(.system(size: 25))
+                            .multilineTextAlignment(.leading)
+                            .bold()
+                            .foregroundColor(.black)
+                    }
+                }
+                VStack {
+                    HStack {
+                        Image(systemName: "clock.fill")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                        Text("Fix your sleep schedule")
+                            .font(.system(size: 22))
+                            .multilineTextAlignment(.leading)
+                            .bold()
+                            .foregroundColor(.black)
+                    }
+                }
+            }
+        }
+        }
 }
 extension Color {
     static let CustomYellowLight = Color (red: 218/255, green: 165/255, blue: 32/255)
