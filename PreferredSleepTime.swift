022d9858ba8f337634563b88c0d5c2929982eee4
Nicole Park <ypark5275@stu.d214.org>
2024-04-25 14:25:21 -0500

    added preferred sleep time view and time picker.

--- PreferredSleepTime.swift
+++ PreferredSleepTime.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import UIKit
+import SwiftUI
+
+struct preferredSleepTime: View {
+    var body: some View {
+        Text("Select preferred sleep time.")
+    }
+}
+
+class sleepTime: UIViewController {
+    @IBOutlet var textfield: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let time = Date()
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_gb")
+        formatter.dateFormat = "hh:mm:ss"
+        textfield.text = formatter.string(from: time)
+        textfield.textColor = .link
+        
+        let timePicker = UIDatePicker()
+        timePicker.datePickerMode = .time
+        timePicker.addTarget(self, action: #selector(timePickerValueChanged(sender:)), for: UIControl.Event.valueChanged)
+        timePicker.frame.size = CGSize(width: 0, height: 250)
+        textfield.inputView = timePicker
+    }
+    @objc func timePickerValueChanged(sender: UIDatePicker)
+    {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_gb")
+        formatter.dateFormat = "hh:mm:ss"
+        textfield.text = formatter.string(from: sender.date)
+    }
+}
