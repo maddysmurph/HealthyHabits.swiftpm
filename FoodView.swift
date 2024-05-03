f3a439d79d11706b22487543d57f84a608b19243
Nicole Park <ypark5275@stu.d214.org>
2024-05-03 14:56:32 -0500

    worked on food inspo button.

---
+++ FoodView.swift
@@ -0,0 +1,18 @@
+//
+//  FoodView.swift
+//  HealthyHabits
+//
+//  Created by Nicole Park on 5/3/24.
+//
+
+import SwiftUI
+
+struct FoodView: View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
+
+#Preview {
+    FoodView()
+}
--- HomeView.swift
+++ HomeView.swift
@@ -23,12 +23,37 @@ struct HomeView: View {
                 .padding([.leading])
             Divider()
             HStack {
-                RoundedRectangle(cornerRadius: 65)
-                    .size(width: 180, height: 265)
-                    .foregroundColor(.CustomGreenLight)
-                    .padding([.leading])
-                    .offset(x: 5, y: 15)
-                    .overlay("Get", alignment: <#T##Alignment#>)
+                ZStack {
+                    RoundedRectangle(cornerRadius: 65)
+                        .size(width: 180, height: 265)
+                        .foregroundColor(.CustomGreenLight)
+                        .padding([.leading])
+                        .offset(x: 5, y: 15)
+                    VStack {
+                        Text("Get")
+                            .font(.system(size: 30))
+                            .foregroundColor(.black)
+                            .offset(x: -25, y: -60)
+                        Text("Food")
+                            .font(.system(size: 40))
+                            .foregroundColor(.CustomGreenDark)
+                            .offset(x: -20, y: -65)
+                            .bold()
+                        Text("Inspo")
+                            .font(.system(size: 30))
+                            .foregroundColor(.black)
+                            .offset(x: -24, y: -72)
+                    }
+                    Button(action: {
+                        FoodView()
+                    }, label: {
+                        Image(systemName: "arrowshape.right.circle.fill")
+                            .resizable()
+                            .frame(width: 60, height: 60)
+                            .foregroundColor(.CustomYellowDark)
+                            .offset(x: 60, y: -40)
+                    })
+                }
                 RoundedRectangle(cornerRadius: 45)
                     .size(width: 150, height: 150)
                     .foregroundColor(.CustomBlueLight)
@@ -49,6 +74,9 @@ struct HomeView: View {
 }
 extension Color {
     static let CustomGreenLight = Color (red: 201/255, green: 238/255, blue: 145/255)
+}
+extension Color {
+    static let CustomGreenDark = Color (red: 34/255, green: 139/255, blue: 34/255)
 }
 extension Color {
     static let CustomPinkLight = Color (red: 255/255, green: 224/255, blue: 233/255)
