00ae19667af525fb0e5b5f3c50441586c97b8048
Neha Darji <ndarji7094@stu.d214.org>
2024-05-13 14:42:14 -0500

    Created one recipe in vegetarianView

--- VegetarianView.swift
+++ VegetarianView.swift
@@ -9,7 +9,56 @@ import SwiftUI
 
 struct VegetarianView: View {
     var body: some View {
-        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+        ZStack{
+            RoundedRectangle(cornerRadius: 45)
+                .size(width: 180, height: 265)
+                .foregroundColor(.CustomBlueLight)
+            HStack{
+                Text("Broccoli Cheddar Soup")
+                    .font(.system(size: 35))
+                    .foregroundColor(.black)
+                    .bold()
+                Text("-Salted Butter")
+                    .font(.system(size: 15))
+                    .foregroundColor(.black)
+                Text("-Onion")
+                    .font(.system(size: 15))
+                    .foregroundColor(.black)
+                Text("-Garlic")
+                    .font(.system(size: 15))
+                    .foregroundColor(.black)
+                Text("-All Purpose Flour")
+                    .font(.system(size: 15))
+                    .foregroundColor(.black)
+                Text("-Sea Salt")
+                    .font(.system(size: 15))
+                    .foregroundColor(.black)
+                Text("-Mustard Powder")
+                    .font(.system(size: 15))
+                    .foregroundColor(.black)
+                Text("-Nutmeg")
+                    .font(.system(size: 15))
+                    .foregroundColor(.black)
+                Text("-Whole milk or half-and-half")
+                    .font(.system(size: 15))
+                    .foregroundColor(.black)
+                Text("-Vegetable Stock")
+                    .font(.system(size: 15))
+                    .foregroundColor(.black)
+                Text("-Broccoli")
+                    .font(.system(size: 15))
+                    .foregroundColor(.black)
+                Text("-Parmesan")
+                    .font(.system(size: 15))
+                    .foregroundColor(.black)
+                Text("-Extra-sharp cheddar cheese")
+                    .font(.system(size: 15))
+                    .foregroundColor(.black)
+                Text("-Black pepper")
+                    .font(.system(size: 15))
+                    .foregroundColor(.black)
+            }
+        }
     }
 }
 
