343c97acc4a47881665eb20b70426ab42a418d45
Neha Darji <ndarji7094@stu.d214.org>
2024-05-13 14:18:32 -0500

    Created new views for different diets and created new navigation link to them in the food view

---
+++ CuttingSugarView.swift
@@ -0,0 +1,18 @@
+//
+//  CuttingSugarView.swift
+//  HealthyHabits
+//
+//  Created by Neha Darji on 5/13/24.
+//
+
+import SwiftUI
+
+struct CuttingSugarView: View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
+
+#Preview {
+    CuttingSugarView()
+}
--- FoodView.swift
+++ FoodView.swift
@@ -11,31 +11,29 @@ import WebKit
 struct FoodView: View {
     var body: some View {
         VStack {
-            HStack {
-                Button(action: {
-                    Link("Go To Recipe", destination: URL(string: "https://www.delish.com/cooking/recipe-ideas/a46332008/chicken-shawarma-bowls-recipe/")!)
-                }, label: {
-                    Image("1")
-                        .resizable()
-                        .clipShape(Rectangle())
-                        .frame(width: 100, height: 500)
-                })
-                Button(action: {
-                    Link("Go To Recipe", destination: URL(string: "https://www.delish.com/cooking/a45877598/indian-butter-chickpeas-recipe/")!)
-                }, label: {
-                    Image("2")
-                        .resizable()
-                        .clipShape(Rectangle())
-                        .frame(width: 100, height: 500)
-                })
-                Button(action: {
-                    Link("Go To Recipe", destination: URL(string: "https://www.delish.com/cooking/recipe-ideas/a27115412/cheeseburger-cabbage-wraps-recipe/")!)
-                }, label: {
-                    Image("3")
-                        .resizable()
-                        .clipShape(Rectangle())
-                        .frame(width: 100, height: 500)
-                })
+            NavigationLink("Vegetarian"){
+                VegetarianView()
+            }
+            NavigationLink("Vegan"){
+                VeganView()
+            }
+            NavigationLink("Keto"){
+                KetoView()
+            }
+            NavigationLink("Balanced"){
+                BalancedView()
+            }
+            NavigationLink("Calorie Control"){
+                CalorieControlView()
+            }
+            NavigationLink("Hydrated"){
+                HydratedView()
+            }
+            NavigationLink("Nutritious"){
+                NutritiousView()
+            }
+            NavigationLink("Cutting Sugar"){
+                CuttingSugarView()
             }
         }
     }
