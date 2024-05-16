cde29eeb30088e0ccc2f6a81c85f9a4cc3d2b88d
Nicole Park <ypark5275@stu.d214.org>
2024-05-16 08:24:06 -0500

    added buttons to exercise views.

--- ProlongLifeSpanView.swift
+++ ProlongLifeSpanView.swift
@@ -9,6 +9,17 @@ import SwiftUI
 
 struct ProlongLifeSpanView: View {
     var body: some View {
+        VStack(alignment: .leading) {
+            Text("Exercises recommended for")
+                .font(.system(size: 30))
+                .foregroundColor(.black)
+                .padding([.leading])
+            Text("Prolonging your Lifespan")
+                .font(.system(size: 50))
+                .foregroundColor(.CustomPinkDark)
+                .bold()
+                .padding([.leading])
+        }
     }
 }
 
--- WeightGainView.swift
+++ WeightGainView.swift
@@ -9,5 +9,56 @@ import SwiftUI
 
 struct WeightGainView: View {
     var body: some View {
+        VStack(alignment: .leading) {
+            Text("Exercises recommended for")
+                .font(.system(size: 30))
+                .foregroundColor(.black)
+                .padding([.leading])
+            Text("Weight Gain")
+                .font(.system(size: 50))
+                .foregroundColor(.CustomPinkDark)
+                .bold()
+                .padding([.leading])
+        }
+        Divider()
+        VStack{
+            HStack(alignment: .center, spacing: 20){
+                Text("Squats")
+                    .frame(width: 300, height: 40)
+                    .font(.system(size: 20))
+                    .bold()
+                    .foregroundColor(.white)
+                    .background(Color.CustomYellowDark)
+                    .clipShape(RoundedRectangle(cornerRadius: 10.0))
+                Text("Push-ups")
+                    .frame(width: 300, height: 40)
+                    .font(.system(size: 20))
+                    .bold()
+                    .foregroundColor(.white)
+                    .background(Color.CustomYellowDark)
+                    .clipShape(RoundedRectangle(cornerRadius: 10.0))
+                Text("Bench press")
+                    .frame(width: 300, height: 40)
+                    .font(.system(size: 20))
+                    .bold()
+                    .foregroundColor(.white)
+                    .background(Color.CustomYellowDark)
+                    .clipShape(RoundedRectangle(cornerRadius: 10.0))
+                Text("Deadlift")
+                    .frame(width: 300, height: 40)
+                    .font(.system(size: 20))
+                    .bold()
+                    .foregroundColor(.white)
+                    .background(Color.CustomYellowDark)
+                    .clipShape(RoundedRectangle(cornerRadius: 10.0))
+                Text("Lunges")
+                    .frame(width: 300, height: 40)
+                    .font(.system(size: 20))
+                    .bold()
+                    .foregroundColor(.white)
+                    .background(Color.CustomYellowDark)
+                    .clipShape(RoundedRectangle(cornerRadius: 10.0))
+            }
+        }
     }
 }
