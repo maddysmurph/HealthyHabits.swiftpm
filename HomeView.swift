f219a64af78b909ccfdd92d2b5bea0503efe2939
Maddy Murphy <mmurphy6284@stu.d214.org>
2024-05-17 14:29:24 -0500

    edited menu along the bottom with navigation links

--- HomeView.swift
+++ HomeView.swift
@@ -106,26 +106,38 @@ struct HomeView: View {
                     }
                 }
             }
-            HStack{
-                NavigationLink{
-                    trackerView()
-                } label: {
-                    Image(systemName: "pin")
-                }
-                
-                NavigationLink{
-                    HomeView()
-                } label: {
-                    Image(systemName: "house")
-                }
-                
-                NavigationLink{
-                    AboutView()
-                }label: {
-                    Image(systemName: "person")
-                }
+            
+        }
+        HStack(spacing: 20){
+            NavigationLink{
+                trackerView()
+            } label: {
+                Image(systemName: "pin")
+                    .resizable()
+                    .frame(width: 40, height: 60)
+                    .foregroundColor(.white)
+            }
+            
+            NavigationLink{
+                HomeView()
+            } label: {
+                Image(systemName: "house.circle")
+                    .resizable()
+                    .frame(width: 85, height: 80)
+                    .foregroundColor(.white)
+            }
+            
+            NavigationLink{
+                AboutView()
+            }label: {
+                Image(systemName: "person")
+                    .resizable()
+                    .frame(width: 55, height: 60)
+                    .foregroundColor(.white)
             }
         }
+        .frame(maxWidth: .infinity, maxHeight: 100)
+        .background(Color.CustomYellowLight)
     }
 }
     extension Color {
