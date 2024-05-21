636701b221c60a939f16281e562e0b526fdbf952
Neha Darji <ndarji7094@stu.d214.org>
2024-05-21 14:29:21 -0500

    Added 5 nutritious recipes

---
+++ NutritiousView.swift
@@ -0,0 +1,59 @@
+//
+//  NutritiousView.swift
+//  HealthyHabits
+//
+//  Created by Neha Darji on 5/21/24.
+//
+
+import SwiftUI
+
+struct NutritiousView: View {
+    let tofuURL = URL(string: "https://www.allrecipes.com/sheet-pan-black-pepper-tofu-and-broccoli-recipe-8649317")!
+    let chickenURL = URL(string: "https://www.allrecipes.com/best-grilled-chicken-breasts-recipe-8648903")!
+    let PearlURL = URL(string: "https://www.allrecipes.com/mediterranean-pearl-couscous-recipe-8646349")!
+    let onionURL = URL(string: "https://www.allrecipes.com/copycat-onion-wrapped-flying-dutchman-recipe-8553215")!
+    let codURL = URL(string: "https://www.allrecipes.com/mediterranean-baked-cod-recipe-8576276")!
+    var body: some View {
+        Button(action: {
+            Link("Sheet Pan Black Pepper Tofu and Broccoli", destination: tofuURL)
+        }, label: {
+            Image("tofu")
+                .resizable()
+                .frame(width: 150, height: 150)
+        })
+        Button(action: {
+            Link("The Best Grilled Chicken Breasts", destination: chickenURL)
+        }, label: {
+            Image("chicken")
+                .resizable()
+                .frame(width: 150, height: 150)
+        })
+        Button(action: {
+            Link("Mediterranean Pearl Couscous", destination: PearlURL)
+        }, label: {
+            Image("pearl")
+                .resizable()
+                .frame(width: 150, height: 150)
+        })
+        Button(action: {
+            Link("Copycat Onion Wrapped Flying Dutchman", destination: onionURL)
+        }, label: {
+            Image("onion")
+                .resizable()
+                .frame(width: 150, height: 150)
+        })
+        Button(action: {
+            Link("Mediterranean Baked Cod", destination: codURL)
+        }, label: {
+            Image("cod")
+                .resizable()
+                .frame(width: 150, height: 150)
+        })
+
+        
+    }
+}
+
+#Preview {
+    NutritiousView()
+}
