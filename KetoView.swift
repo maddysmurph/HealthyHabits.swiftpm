0676c3186ca0b44815eac87caeb901c0aa75023a
Nicole Park <ypark5275@stu.d214.org>
2024-05-23 14:46:49 -0500

    fixed layout for recipe view

--- KetoView.swift
+++ KetoView.swift
@@ -14,47 +14,73 @@ struct KetoView: View {
     let baconURL = URL(string: "https://www.foodnetwork.com/recipes/keto-bacon-cheeseburger-bowls-9584601")!
     let pizzaURL = URL(string: "https://www.foodnetwork.com/recipes/food-network-kitchen/keto-pizza-snacks-5225642")!
     var body: some View {
-        Button(action: {
-            Link("Bell Pepper Keto Nachos", destination: nachosURL)
-        }, label: {
-            Image("11")
-                .resizable()
-                .frame(width: 150, height: 150)
-        })
-        Link("Bell Pper Keto Nachos", destination: URL(string: "https://www.foodnetwork.com/recipes/food-network-kitchen/bell-pepper-keto-nachos-5224931")!)
-        Button(action: {
-            Link("Keto Sheet Pan Chicken and Loaded Cauliflower", destination: sheetURL)
-        }, label: {
-            Image("12")
-                .resizable()
-                .frame(width: 150, height: 150)
-        })
-        Link("Keto Sheet Pan Chicken and Loaded Cauliflower", destination: URL(string: "https://www.foodnetwork.com/recipes/food-network-kitchen/keto-sheet-pan-chicken-and-loaded-cauliflower-8050489")!)
-        Button(action: {
-            Link("Gluten Free Keto Meatloaf", destination: glutenURL)
-        }, label: {
-            Image("13")
-                .resizable()
-                .frame(width: 150, height: 150)
-        })
-        Link("Gluten Free Keto Meatloaf", destination: URL(string: "https://www.foodnetwork.com/recipes/food-network-kitchen/gluten-free-keto-meatloaf-7579578")!)
-        Button(action: {
-            Link("Keto Bacon Cheeseburger Bowls", destination: baconURL)
-        }, label: {
-            Image("14")
-                .resizable()
-                .frame(width: 150, height: 150)
-        })
-        Link("Keto Bacon Cheeseburger Bowls", destination: URL(string: "https://www.foodnetwork.com/recipes/keto-bacon-cheeseburger-bowls-9584601")!)
-        Button(action: {
-            Link("Keto Pizza Snack", destination: pizzaURL)
-        }, label: {
-            Image("15")
-                .resizable()
-                .frame(width: 150, height: 150)
-        })
-        Link("Keto Pizza Snack", destination: URL(string: "https://www.foodnetwork.com/recipes/food-network-kitchen/keto-pizza-snacks-5225642")!)
-    }
-}
+        ScrollView {
+            VStack(alignment: .center) {
+                HStack {
+                    VStack {
+                        Button(action: {
+                            Link("Bell Pepper Keto Nachos", destination: nachosURL)
+                        }, label: {
+                            Image("11")
+                                .resizable()
+                                .frame(width: 150, height: 150)
+                                .clipShape(RoundedRectangle(cornerRadius: 10.0))
+                        })
+                        Link("Bell Pepper Keto Nachos", destination: URL(string: "https://www.foodnetwork.com/recipes/food-network-kitchen/bell-pepper-keto-nachos-5224931")!)
+                    }
+                    VStack {
+                        Button(action: {
+                        Link("Keto Sheet Pan Chicken and Loaded Cauliflower", destination: sheetURL)
+                    }, label: {
+                        Image("12")
+                            .resizable()
+                            .frame(width: 150, height: 150)
+                            .clipShape(RoundedRectangle(cornerRadius: 10.0))
+                    })
+                        Link("Keto Sheet Pan Chicken & Loaded Cauliflower", destination: URL(string: "https://www.foodnetwork.com/recipes/food-network-kitchen/keto-sheet-pan-chicken-and-loaded-cauliflower-8050489")!)
+                            }
+                }
+                HStack {
+                    VStack {
+                        Button(action: {
+                            Link("Gluten Free Keto Meatloaf", destination: glutenURL)
+                        }, label: {
+                            Image("13")
+                                .resizable()
+                                .frame(width: 150, height: 150)
+                                .clipShape(RoundedRectangle(cornerRadius: 10.0))
+                        })
+                        Link("Gluten Free Keto Meatloaf", destination: URL(string: "https://www.foodnetwork.com/recipes/food-network-kitchen/gluten-free-keto-meatloaf-7579578")!)
+                    }
+                    VStack {
+                        Button(action: {
+                            Link("Keto Bacon Cheeseburger Bowls", destination: baconURL)
+                        }, label: {
+                            Image("14")
+                                .resizable()
+                                .frame(width: 150, height: 150)
+                                .clipShape(RoundedRectangle(cornerRadius: 10.0))
+                        })
+                        Link("Keto Bacon Cheeseburger Bowls", destination: URL(string: "https://www.foodnetwork.com/recipes/keto-bacon-cheeseburger-bowls-9584601")!)
+                    }
+                }
+                HStack {
+                    VStack {
+                        Button(action: {
+                            Link("Keto Pizza Snack", destination: pizzaURL)
+                        }, label: {
+                            Image("15")
+                                .resizable()
+                                .frame(width: 150, height: 150)
+                                .clipShape(RoundedRectangle(cornerRadius: 10.0))
+                        })
+                        Link("Keto Pizza Snack", destination: URL(string: "https://www.foodnetwork.com/recipes/food-network-kitchen/keto-pizza-snacks-5225642")!)
+                    }
+                }
+            }
+        }
 
 
+
+            }
+        }
--- VegetarianView.swift
+++ VegetarianView.swift
@@ -15,54 +15,73 @@ struct VegetarianView: View {
     let creamyURL = URL(string: "https://themodernproper.com/creamy-cavatappi")!
     
     var body: some View {
-        VStack {
-            Button(action: {
-                Link("Kung Pao Caudiflower", destination: KungPaoURL)
-            }, label: {
-                Image("1")
-                    .resizable()
-                    .frame(width: 150, height: 150)
-            })
-            Link("Kung Pao Caudiflower", destination: URL(string: "https://www.foodnetwork.com/recipes/food-network-kitchen/kung-pao-cauliflower-5339620")!)
-            
-            Button(action: {
-                Link("Fusilli Pasta with Spicy Red Tomatoe Sauce", destination: URL(string: "https://themodernproper.com/fusilli-pasta-with-spicy-tomato-sauce")!)
-            }, label: {
-                Image("2")
-                    .resizable()
-                    .frame(width: 150, height: 150)
-            })
-            
-            Link("Fusilli Pasta with Spicy Red Tomatoe Sauce", destination: URL(string: "https://themodernproper.com/fusilli-pasta-with-spicy-tomato-sauce")!)
-            
-            Button(action: {
-                Link("Brocolli Cheddar Soup", destination: BrocolliURL)
-            }, label: {
-                Image("3")
-                    .resizable()
-                    .frame(width: 150, height: 150)
-            })
-            
-            Link("Brocolli Cheddar Soup", destination: URL(string: "https://themodernproper.com/broccoli-cheddar-soup")!)
-            
-            Button(action: {
-                Link("7 Layer Dip", destination: SevenLayerURL)
-            }, label: {
-                Image("4")
-                    .resizable()
-                    .frame(width: 150, height: 150)
-            })
-            
-            Link("7 Layer Dip", destination: URL(string: "https://themodernproper.com/seven-layer-dip")!)
-            
-            Button(action: {
-                Link("Creamy Cavatappi", destination: URL(string: "https://themodernproper.com/creamy-cavatappi")!)
-            }, label: {
-                Image("5")
-                    .resizable()
-                    .frame(width: 150, height: 150)
-            })
-            Link("Creamy Cavatappi", destination: URL(string: "https://themodernproper.com/creamy-cavatappi")!)
+        ScrollView {
+            VStack(alignment: .center) {
+                HStack {
+                    VStack {
+                        Button(action: {
+                            Link("Kung Pao Caudiflower", destination: KungPaoURL)
+                        }, label: {
+                            Image("1")
+                                .resizable()
+                                .frame(width: 150, height: 150)
+                                .clipShape(RoundedRectangle(cornerRadius: 10.0))
+                        })
+                        Link("Kung Pao Caudiflower", destination: URL(string: "https://www.foodnetwork.com/recipes/food-network-kitchen/kung-pao-cauliflower-5339620")!)
+                    }
+                    VStack {
+                        Button(action: {
+                            Link("Fusilli Pasta with Spicy Red Tomatoe Sauce", destination: URL(string: "https://themodernproper.com/fusilli-pasta-with-spicy-tomato-sauce")!)
+                        }, label: {
+                            Image("2")
+                                .resizable()
+                                .frame(width: 150, height: 150)
+                                .clipShape(RoundedRectangle(cornerRadius: 10.0))
+                        })
+                        
+                        Link("Fusilli Pasta with Spicy Red Tomatoe Sauce", destination: URL(string: "https://themodernproper.com/fusilli-pasta-with-spicy-tomato-sauce")!)
+                    }
+            HStack {
+                VStack {
+                    Button(action: {
+                        Link("Brocolli Cheddar Soup", destination: BrocolliURL)
+                            }, label: {
+                                Image("3")
+                                    .resizable()
+                                    .frame(width: 150, height: 150)
+                                    .clipShape(RoundedRectangle(cornerRadius: 10.0))
+                            })
+                            
+                        Link("Brocolli Cheddar Soup", destination: URL(string: "https://themodernproper.com/broccoli-cheddar-soup")!)
+                        }
+                VStack {
+                    Button(action: {
+                        Link("7 Layer Dip", destination: SevenLayerURL)
+                            }, label: {
+                                Image("4")
+                            .resizable()
+                            .frame(width: 150, height: 150)
+                            .clipShape(RoundedRectangle(cornerRadius: 10.0))
+                            })
+                            
+                        Link("7 Layer Dip", destination: URL(string: "https://themodernproper.com/seven-layer-dip")!)
+                        }
+                    }
+            HStack {
+                VStack {
+                    Button(action: {
+                        Link("Creamy Cavatappi", destination: URL(string: "https://themodernproper.com/creamy-cavatappi")!)
+                            }, label: {
+                        Image("5")
+                            .resizable()
+                            .frame(width: 150, height: 150)
+                            .clipShape(RoundedRectangle(cornerRadius: 10.0))
+                            })
+                            Link("Creamy Cavatappi", destination: URL(string: "https://themodernproper.com/creamy-cavatappi")!)
+                        }
+                    }
+                }
+            }
         }
         }
     }
